1c55838213917885654a9ec66b18d4454d84dcc2
Victoria Reyes <reyedvictoria1803@gmail.com>
2025-03-10 19:31:21 +0000

    server

--- sakila/settings.py
+++ sakila/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=^9kl4r_^ghzjjfibrog+$_!5ouo@tb$13j*!l4_e5k&k$y8ud'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
