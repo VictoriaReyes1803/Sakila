22dccb14953cab9daaa528e915501c6474ad0348
Alonso <abrahamreynoso64@gmail.com>
2025-04-04 00:05:03 -0600

    Cambiando el puerto de resend

--- sakila/settings.py
+++ sakila/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = ['*']
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-RESEND_SMTP_PORT = 587
+RESEND_SMTP_PORT = 2587
 RESEND_SMTP_USERNAME = 'resend'
 RESEND_SMTP_HOST = 'smtp.resend.com'
 
